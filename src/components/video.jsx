4ade83c250d60240e31ce1a64480af253390a504
saurondevs <113474242+saurondevs@users.noreply.github.com>
2022-09-13 23:46:15 +0200

    Update video.jsx

--- src/components/video.jsx
+++ src/components/video.jsx
@@ -17,7 +17,7 @@ import { Box, Image } from '@chakra-ui/react'
 const url = 'https://sauronbucket.s3.amazonaws.com/sauron.gif'
 
 export const Video = () => {
-  console.log("url")
+  console.log(url)
   return(
   <Box position={'absolute'} zIndex={-1} height={'100vh'} width={'100vw'}>
     <Image src={url} boxSize={'100%'} objectFit='cover'/>
